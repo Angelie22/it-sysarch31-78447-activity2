4b7d26b7718b3f27b9e95ea35e0cc0154b4bfef0
gelay <angeliembalugo@gmail.com>
2024-03-10 22:11:45 +0800

    activity 2

---
+++ src/Card.jsx
@@ -0,0 +1,43 @@
+function Card(){
+    return(
+        <card>
+            <div className="card">
+                <img src="https://d2v0j9zp5u17nn.cloudfront.net/wp-content/uploads/2024/02/13140345/daa.jpg" alt="Trulli" width="150" height="150" ></img>
+                <h3>Baek Ha Rin</h3>
+                <p>baekHaRin@gmail.com</p>
+            </div>
+
+            <div className="card">
+                <img src="https://6.soompi.io/wp-content/uploads/image/20240208122831_pyramid-game-4.jpg?s=900x600&e=t" alt="Trulli" width="150" height="150" ></img>
+                <h3>Sung Soo Ji</h3>
+                <p>sungSooJi@gmail.com</p>
+            </div>
+
+            <div className="card">
+                <img src="https://photos.hancinema.net/photos/photo1616045.jpg" alt="Trulli" width="150" height="150" ></img>
+                <h3>Myung Ja Eun</h3>
+                <p>myungJaEun@gmail.com</p>
+            </div>
+
+            <div className="card">
+                <img src="https://i.mydramalist.com/kAV8pm_7f.jpg" alt="Trulli" width="150" height="150" ></img>
+                <h3>Im Ye Rim</h3>
+                <p>imYeRim@gmail.com</p>
+            </div>
+
+            <div className="card">
+                <img src="https://asianwiki.com/images/8/85/Ha_Yul-Ri-1999-pt1.jpeg" alt="Trulli" width="150" height="150" ></img>
+                <h3>Bang Woo Yi</h3>
+                <p>bangWooYi@gmail.com</p>
+            </div>
+
+            <div className="card">
+                <img src="https://i.mydramalist.com/XdAVkn_5c.jpg" alt="Trulli" width="150" height="150" ></img>
+                <h3>Kim Da Yun</h3>
+                <p>kimDaYun@gmail.com</p>
+            </div>
+
+        </card>
+    );
+}
+export default Card
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p className="footerP">&copy; {new Date().getFullYear()} IT-SYSARCH31-78447 Made by Angelie Balugo</p>
+        </footer>
+    );
+}
+export default Footer
